b1a2745d5bd01f863c8217abfa68a928ca69988b
CristianER21 <cristian.elgueta@agsbyte.cl>
2025-06-13 12:58:18 -0400

    node test yml changes

---
+++ .eslintrc.js
@@ -0,0 +1,17 @@
+module.exports = {
+    env: {
+      node: true,
+      es2021: true,
+    },
+    extends: ['eslint:recommended'],
+    parserOptions: {
+      ecmaVersion: 'latest',
+      sourceType: 'module',
+    },
+    rules: {
+      semi: ['error', 'always'],
+      'no-console': 'warn',
+      'no-unused-vars': 'warn',
+    },
+  };
+  
